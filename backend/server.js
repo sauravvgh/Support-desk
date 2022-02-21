57610fc9a09948c10781845abc18bf12e9d97d83
saurav ghimire <saurav.ghimire@zakipoint.com>
2022-02-21 19:27:38 +0545

    committed again

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-console.log('server...')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const { connect } = require('mongoose')
+const PORT = process.env.PORT || 5000
+
+
+//Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res)=>{
+    res.send('Hello')
+})
+
+
+
+//Routes
+app.use('/api/users/', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, ()=> console.log(`server started on port ${PORT}`))
+
