81c47f2c8e9d158c67df19ed69ad35161f338784
saurav ghimire <saurav.ghimire@zakipoint.com>
2022-02-26 19:52:47 +0545

    committed front end

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
+
+function App() {
+  return (
+    <>
+     <Router>
+      <div className="container">
+        <Routes>
+          <Route path='/' element= {<Home />} />
+          <Route path='/login' element= {<Login />} />
+          <Route path='/register' element= {<Register />} />
+        </Routes>
+      </div>
+  </Router>
+    </>
+  );
+}
+
+export default App;
